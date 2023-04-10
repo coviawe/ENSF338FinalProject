b0db4846082491ee4360984688bb11a17b4dc3bc
emiko47 <113942291+emiko47@users.noreply.github.com>
2023-04-09 20:50:21 -0600

    Update BST.java

--- myLib/datastructures/trees/BST.java
+++ myLib/datastructures/trees/BST.java
@@ -1,8 +1,8 @@
 public class BST {
-    private TNode root;
+    protected TNode root;
     
     public BST() {
-        root = null;
+        this.root = null;
     }
     
     public BST(int val) {
@@ -14,7 +14,7 @@ public class BST {
     }
     
     public TNode getRoot() {
-        return root;
+        return this.root;
     }
     
     public void setRoot(TNode root) {
@@ -76,4 +76,92 @@ public class BST {
         }
     }
     
-    public void
+    public void delete(int val) {
+        if (root == null) {
+        System.out.println("The tree is empty.");
+        return;
+        }
+    
+        TNode curr = root;
+        TNode parent = null;
+        boolean isLeftChild = false;
+    
+        // find the node to be deleted
+        while (curr != null && curr.getData() != val) {
+            parent = curr;
+            if (val < curr.getData()) {
+                curr = curr.getLeft();
+                isLeftChild = true;
+            } else {
+                curr = curr.getRight();
+                isLeftChild = false;
+            }
+        }
+    
+    // if the node was not found
+        if (curr == null) {
+            System.out.println("Value not found in the tree.");
+            return;
+        }
+    
+    // if the node has no children
+        if (curr.getLeft() == null && curr.getRight() == null) {
+            if (curr == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.setLeft(null);
+            } else {
+                parent.setRight(null);
+            }
+        }
+    // if the node has one child
+        else if (curr.getRight() == null) {
+            if (curr == root) {
+                root = curr.getLeft();
+            } else if (isLeftChild) {
+                parent.setLeft(curr.getLeft());
+            } else {
+                parent.setRight(curr.getLeft());
+            }
+        } else if (curr.getLeft() == null) {
+            if (curr == root) {
+                root = curr.getRight();
+            } else if (isLeftChild) {
+                parent.setLeft(curr.getRight());
+            } else {
+                parent.setRight(curr.getRight());
+            }
+        }
+    // if the node has two children
+        else {
+            TNode successor = getSuccessor(curr);
+            if (curr == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.setLeft(successor);
+            } else {
+                parent.setRight(successor);
+            }
+            successor.setLeft(curr.getLeft());
+        }
+        }
+
+// helper method to get the successor of a given node
+    private TNode getSuccessor(TNode node) {
+        TNode successorParent = node;
+        TNode successor = node;
+        TNode curr = node.getRight();
+    
+        while (curr != null) {
+            successorParent = successor;
+            successor = curr;
+            curr = curr.getLeft();
+        }
+    
+        if (successor != node.getRight()) {
+            successorParent.setLeft(successor.getRight());
+            successor.setRight(node.getRight());
+        }
+    
+        return successor;
+}
